c42f96f6b223de6f2fe2ffa5b303fcac114ead76
SfumatoCannon <3075964583@qq.com>
2025-11-29 03:05:20 +0800

    jump format class

---
+++ YAlignCalculator/JumpFormat.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAlignCalculator
+{
+    internal class JumpFormat
+    {
+        public bool IsJumped { get; set; }
+        public List<int> CancelFrameList { get; set; }
+        public String GetJumpExpression()
+        {
+            if (!IsJumped)
+                return null;
+            if (CancelFrameList.Count == 0)
+                return "23f";
+            else if (CancelFrameList.Count == 1)
+                return $"{CancelFrameList[0]}f";
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"({CancelFrameList[0]}");
+            foreach (int i in CancelFrameList)
+                sb.Append($" + {i}");
+            sb.Append(")");
+            return sb.ToString();
+        }
+        public bool IsFullJump { get { return IsJumped && CancelFrameList.Count == 0; } }
+        public bool IsNormalJump { get { return IsJumped && CancelFrameList.Count <= 1; } }
+        public bool IsAdvancedJump { get { return IsJumped && CancelFrameList.Count >= 2; } }
+        public bool IsJumpCancel { get { return IsAdvancedJump && CancelFrameList[0] == 1; } }
+        public bool IsCactus { get { return IsAdvancedJump && CancelFrameList[0] >= 2; } }
+    }
+}
---
+++ YAlignCalculator/YAlignAdjustCalculation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAlignCalculator
+{
+    internal class YAlignAdjustCalculation
+    {
+        public float StartY { get; set; }
+        public float TargetYLeft { get; set; }
+        public float TargetYRight { get; set; }
+        public enum HeightLimitTypeEnum { None, Solid, Deadly };
+        public HeightLimitTypeEnum HeightLimitType { get; set; }
+        public int HeightLimitY { get; set; }
+        public enum SearchStrategyEnum { Easiest, Fastest };
+        public SearchStrategyEnum SearchStrategy { get; set; }
+
+        public class AdditionalSettingsClass
+        {
+            public bool CanCactus { get; set; }
+            public bool CanJumpCancel { get; set; }
+            public bool PlatformLanding { get; set; }
+            public bool SingleJumpOnly { get; set; }
+            public bool NoBunnyHop { get; set; }
+        };
+        public AdditionalSettingsClass AdditionalSettings { get; set; }
+    }
+}

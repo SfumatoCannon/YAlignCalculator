c599c8718ef82bf1ba1977f625610f9893871656
SfumatoCannon <3075964583@qq.com>
2025-11-30 01:57:40 +0800

    add LeapFormat class, 完善相关时间点/时长计算方法

--- YAlignCalculator/JumpFormat.cs
+++ YAlignCalculator/JumpFormat.cs
@@ -25,6 +25,23 @@ namespace YAlignCalculator
             sb.Append(")");
             return sb.ToString();
         }
+        public int GetCancelFrameTick(int cancelIndex) // CancelIndex start from 0; Return start from 1
+        {
+            if (cancelIndex < 0 || cancelIndex >= CancelFrameList.Count )
+            {
+                throw new ArgumentOutOfRangeException(nameof(cancelIndex), cancelIndex, $"'{nameof(cancelIndex)}' cannot be negative");
+            }
+            int result = 0;
+            for (int i = 0; i <= cancelIndex; i++)
+                result += CancelFrameList[i];
+            return result;
+        }
+        public int GetTotalDuration()
+        {
+            if (CancelFrameList.Count == 0)
+                return 0;
+            return GetCancelFrameTick(CancelFrameList.Count - 1);
+        }
         public bool IsFullJump { get { return IsJumped && CancelFrameList.Count == 0; } }
         public bool IsNormalJump { get { return IsJumped && CancelFrameList.Count <= 1; } }
         public bool IsAdvancedJump { get { return IsJumped && CancelFrameList.Count >= 2; } }
---
+++ YAlignCalculator/LeapFormat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YAlignCalculator
+{
+    internal class LeapFormat
+    {
+        public List<JumpFormat> JumpDeatilList { get; set; } 
+        public List<int> JumpPauseList { get; set; } // JumpPauseList[0] is always 1
+                                                     // JumpPauseList[i] start from 1
+        public int JumpCount { get { return JumpDeatilList.Count; } }
+        public int GetJumpFrameTick(int jumpIndex) // JumpIndex start from 0; Result start From 1 
+        {
+            if (jumpIndex < 0 || jumpIndex >= JumpDeatilList.Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(jumpIndex), jumpIndex, $"'{nameof(jumpIndex)}' cannot be negative");
+            }
+            int result = 1;
+            for (int i = 0; i <= jumpIndex - 1; i++)
+                result += JumpDeatilList[i].GetTotalDuration() + JumpPauseList[i] - 1;
+            result += JumpPauseList[jumpIndex] - 1;
+            return result;
+        }
+    }
+}
